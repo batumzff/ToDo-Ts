3d615251f1691cbd0e51b7e9da21275385118667
batumzff <batuhanmuzafferoglu@gmail.com>
2024-08-14 15:59:41 +0300

    item

---
+++ src/components/Item.tsx
@@ -0,0 +1,99 @@
+import type { Todo } from '../schemas/todos'
+
+import { useEffect, useState } from 'react'
+import { flushSync } from 'react-dom'
+
+import { Checkbox } from './Checkbox'
+import { Menubar } from './Menubar'
+import { useTodoDispatchContext } from '../hooks/useDispatchContext'
+import { EDIT_TODO_DELAY } from '../lib/constants'
+import { cn } from '../lib/utils'
+
+import './Item.css'
+
+type ItemProps = {
+  item: Todo
+}
+
+export const Item = ({ item }: ItemProps) => {
+  const dispatch = useTodoDispatchContext()
+
+  const [isEditing, setIsEditing] = useState(false)
+  const [newTitle, setNewTitle] = useState(item.title)
+
+  function onCheckedChange(checked: boolean) {
+    dispatch({
+      type: 'CHECK_TODO',
+      payload: { id: item.id, isCompleted: checked },
+    })
+  }
+
+  function onEdit() {
+    setIsEditing(!isEditing)
+  }
+
+  function onDelete() {
+    if (document.startViewTransition) {
+      document.startViewTransition(() => {
+        flushSync(() => {
+          dispatch({ type: 'DELETE_TODO', payload: { id: item.id } })
+        })
+      })
+    } else {
+      dispatch({ type: 'DELETE_TODO', payload: { id: item.id } })
+    }
+  }
+
+  useEffect(() => {
+    let timeout: number | null = null
+
+    if (isEditing && newTitle !== item.title) {
+      timeout = setTimeout(() => {
+        dispatch({
+          type: 'EDIT_TODO',
+          payload: { id: item.id, title: newTitle },
+        })
+      }, EDIT_TODO_DELAY)
+    }
+
+    return () => {
+        if(timeout) {
+            clearTimeout(timeout)
+        }
+    }
+  }, [dispatch, isEditing, item.id, item.title, newTitle])
+
+  return (
+    <li 
+        className="flex items-center gap-3 px-4 py-3 border border-gray-800 rounded-md"
+        style={{
+            viewTransitionName: `item-${item.id}`,
+        }}
+        >
+            <Checkbox checked={item.isCompleted} onCheckedChange={onCheckedChange}/>
+            {isEditing ? (
+                <input 
+                type="text"
+                value={newTitle}
+                autoFocus
+                onChange={(event) => setNewTitle(event.target.value)}
+                onBlur={()=> setIsEditing(false)}
+                placeholder="Cook food"
+                aria-label="Edit title"
+                className="text-base bg-transparent font-medium text-gray-800 placeholder:opacity-80 focus:outline-none flex-grow"
+
+                 />
+            ) : (
+                <p
+                className={cn('text-base font-medium text-gray-800 flex-grow', {
+                    'line-through': item.isCompleted,
+                })}
+                >
+                    {item.title}
+                </p>
+            )}
+
+            <Menubar onEdit={onEdit} onDelete={onDelete} />
+        </li>
+  )
+}
---
+++ src/components/index.tsx
@@ -0,0 +1,2 @@
+export { Header } from './Header'
+export { FormInput } from './FormInput'
